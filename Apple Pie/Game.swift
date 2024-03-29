aa40732a58f6dd7499d870ee0ad7b21e115fe2ad
wangsh65 <wangsh@ssfs.org>
2024-03-29 14:15:29 -0400

    new one

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Shelly on 3/29/24.
+//
+
+import Foundation
+
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
+
