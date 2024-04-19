c4e84baa029de0fbea383f77f1599e8441afe1aa
wangsh65 <wangsh@ssfs.org>
2024-04-19 11:54:54 -0400

    Apple tree (new)

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -12,24 +12,28 @@ struct Game {
     var word: String
     var incorrectMovesRemaining: Int
     var guessedLetters: [Character]
+
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter){
+                guessedWord += "\(letter)"
+            }else{
+                guessedWord += "_"
+                }
+            }
+            return guessedWord
+        }
+    
     mutating func playerGuessed(letter: Character) {
         guessedLetters.append(letter)
         if !word.contains(letter) {
-        incorrectMovesRemaining -= 1
+            incorrectMovesRemaining -= 1
         }
     }
 }
-var formattedWord: String {
-    var guessedWord = ""
-    for letter in word {
-        if guessedLetters.contains(letter){
-            guessedWord += "\(letter)"
-        }else{
-            guessedWord += "_"
-        }
-    }
-    return guessedWord
-}
+
+
 
 
 
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -60,3 +60,4 @@ class ViewController: UIViewController {
 
 
 
+}
